ea8fd3fd4406a033b042be2b34aee97f41d70506
beccaj801 <becca.jeppson@gmail.com>
2025-04-17 20:50:18 -0600

    Fav Things Java Script

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+document: refers ti tge current document
+getElementById: it gets the element based on id value
+src: tells you the source
+function: saves your code for later
+addEventListener: Add an event to any object
+*/
+
+
+document.getElementById("pickle").onclick= function(){
+document.getElementById("displayImage").src="images/pickleball.jpg";
+
+}
+
+document.getElementById("coffee").onclick= function(){
+    document.getElementById("displayImage").src="images/coffee.jpg";
+    
+}
+
+document.getElementById("candy").onclick= function(){
+    document.getElementById("displayImage").src="images/candy.jpg";
+    
+}
